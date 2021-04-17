43b3dc5753b835052c9d4836885f7365b28d5e3b
Robin van der Linde <robin.vanderlinde@gmail.com>
2021-04-17 20:10:19 +0200

    🎉 First basic website version

---
+++ website/index.ts
@@ -0,0 +1,73 @@
+"use strict";
+
+import Chart from 'chart.js/auto';
+import 'bootstrap/js/dist/collapse';
+
+(function () {
+  window.onload = function () {
+    const dropdown = document.getElementById("city-dropdown");
+
+    dropdown.onchange = (ev: Event) => {
+      var city = (<HTMLInputElement>ev.srcElement).value; 
+      if (window.myChart1 != null) {
+        window.myChart1.destroy();
+      }
+    
+      const requestURL = city + ".json";
+      let request = new XMLHttpRequest();
+      request.open("GET", requestURL);
+      request.responseType = "json";
+      request.send();
+    
+      request.onload = function () {
+        const requestData = request.response;
+        var ctx = document.getElementById("statsChart").getContext("2d");
+        window.myChart1 = new Chart (ctx, {
+          type: "line",
+          data: requestData,
+          options: {
+            responsive: true,
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+            },
+          },
+        });
+      };
+    }
+
+    let defaultOption = document.createElement("option");
+    defaultOption.text = "Choose a city";
+
+    dropdown.add(defaultOption);
+    dropdown.selectedIndex = 0;
+
+    const requestURL = "cities.json";
+    let request = new XMLHttpRequest();
+    request.open("GET", requestURL, true);
+    request.responseType = "json";
+    request.send();
+
+    request.onload = function () {
+      const requestData = request.response;
+      const countries = Object.keys(requestData);
+
+      let option;
+
+      for (var i = 0; i < countries.length; i++) {
+        var currentCountry = countries[i];
+        var cities = Object.keys(requestData[currentCountry]);
+
+        for (var j = 0; j < cities.length; j++) {
+          var currentCity = cities[j];
+
+          option = document.createElement("option");
+          option.text = requestData[currentCountry][currentCity]["name"];
+          option.value = currentCountry + "/" + currentCity;
+          dropdown.add(option);
+        }
+      }
+    };
+  };
+})();
