391cd07535310a91207243ffed5084c62c00cefe
Robin van der Linde <robin.vanderlinde@gmail.com>
2021-04-23 09:44:53 +0200

    ✨ Update website to show sources

--- website/src/index.ts
+++ website/src/index.ts
@@ -1,6 +1,7 @@
 "use strict";
 
 import Chart from '../node_modules/chart.js/auto';
+import 'chartjs-adapter-date-fns';
 
 (function () {
   window.onload = function () {
@@ -28,15 +29,142 @@ import Chart from '../node_modules/chart.js/auto';
           var ctx = document.getElementById("statsChart").getContext("2d");
           window.myChart1 = new Chart (ctx, {
             type: "line",
-            data: requestData,
+            data: {
+              datasets: [
+                {
+                  label: "Female (cis)",
+                  data: requestData['statistics'],
+                  backgroundColor: '#800080',
+                  borderColor: '#800080',
+                  parsing: {
+                    yAxisKey: 'F'
+                  }
+                },
+                {
+                  label: "Male (cis)",
+                  data: requestData['statistics'],
+                  backgroundColor: '#C8C800',
+                  borderColor: '#C8C800',
+                  parsing: {
+                    yAxisKey: 'M'
+                  }
+                },
+                {
+                  label: "Female (trans)",
+                  data: requestData['statistics'],
+                  backgroundColor: '#00A050',
+                  borderColor: '#00A050',
+                  parsing: {
+                    yAxisKey: 'FX'
+                  }
+                },
+                {
+                  label: "Male (trans)",
+                  data: requestData['statistics'],
+                  backgroundColor: '#00A050',
+                  borderColor: '#00A050',
+                  parsing: {
+                    yAxisKey: 'MX'
+                  }
+                },
+                {
+                  label: "Intersex",
+                  data: requestData['statistics'],
+                  backgroundColor: '#00A050',
+                  borderColor: '#00A050',
+                  parsing: {
+                    yAxisKey: 'X'
+                  }
+                },
+                {
+                  label: "Non-binary",
+                  data: requestData['statistics'],
+                  backgroundColor: '#808080',
+                  borderColor: '#808080',
+                  parsing: {
+                    yAxisKey: 'NB'
+                  }
+                },
+                {
+                  label: "Multiple",
+                  data: requestData['statistics'],
+                  backgroundColor: '#A46440',
+                  borderColor: '#A46440',
+                  parsing: {
+                    yAxisKey: '+'
+                  }
+                },
+                {
+                  label: "Unknown",
+                  data: requestData['statistics'],
+                  backgroundColor: '#808080',
+                  borderColor: '#808080',
+                  parsing: {
+                    yAxisKey: '?'
+                  }
+                },
+                {
+                  label: "Not a person",
+                  data: requestData['statistics'],
+                  backgroundColor: '#DDDDDD',
+                  borderColor: '#DDDDDD',
+                  parsing: {
+                    yAxisKey: '-'
+                  }
+                },
+                {
+                  label: "Wikidata",
+                  data: requestData['sources'],
+                  borderDash: [5, 5],
+                  backgroundColor: '#990000',
+                  borderColor: '#990000',
+                  parsing: {
+                    yAxisKey: 'wikidata'
+                  }
+                },
+                {
+                  label: "From config file",
+                  data: requestData['sources'],
+                  borderDash: [5, 5],
+                  backgroundColor: '#1000FF',
+                  borderColor: '#1000FF',
+                  parsing: {
+                    yAxisKey: 'config'
+                  }
+                },
+                {
+                  label: "Event",
+                  data: requestData['sources'],
+                  borderDash: [5, 5],
+                  backgroundColor: '#00A1FF',
+                  borderColor: '#00A1FF',
+                  parsing: {
+                    yAxisKey: 'event'
+                  }
+                },
+                {
+                  label: "Not mapped",
+                  data: requestData['sources'],
+                  borderDash: [5, 5],
+                  backgroundColor: '#DDDDDD',
+                  borderColor: '#DDDDDD',
+                  parsing: {
+                    yAxisKey: '-'
+                  }
+                }
+              ]
+            },
             options: {
               responsive: true,
               scales: {
+                x: {
+                  type: 'time'
+                },
                 y: {
-                  beginAtZero: true,
+                  beginAtZero: true
                 },
               },
-            },
+            }
           });
         };
       }
